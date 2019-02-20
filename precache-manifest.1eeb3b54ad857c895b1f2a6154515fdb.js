db0552f8862f7182770b79cfbeec979bb47298ee
Łukasz Tymiński <lttyminski@gmail.com>
2019-02-20 09:53:26 +0100

    Updates

---
+++ precache-manifest.1eeb3b54ad857c895b1f2a6154515fdb.js
@@ -0,0 +1,55 @@
+self.__precacheManifest = [
+  {
+    "revision": "72f15fa766bc05a4b3ecaa8579763f85",
+    "url": "public/fonts/fa-regular-400.woff"
+  },
+  {
+    "revision": "c8ea4c79af12c22b2b6073999137156c",
+    "url": "public/fonts/fa-solid-900.svg"
+  },
+  {
+    "revision": "20c189aa192501e4ec907d5f84e8fbb1",
+    "url": "public/fonts/fa-solid-900.ttf"
+  },
+  {
+    "revision": "9c73abbdbd6492778680163269b68345",
+    "url": "public/fonts/fa-solid-900.woff"
+  },
+  {
+    "revision": "3638e62ea50e6f5859b6a15276c25c87",
+    "url": "public/fonts/fa-solid-900.woff2"
+  },
+  {
+    "revision": "9a52a4e971938c52d6c541b9bf3dc2ec",
+    "url": "public/fonts/fa-solid-900.eot"
+  },
+  {
+    "revision": "6a9d786e67d54419d8629081fbb555d6",
+    "url": "public/fonts/fa-regular-400.woff2"
+  },
+  {
+    "revision": "fcb220ee57704c9c80680153bada946c",
+    "url": "public/fonts/fa-regular-400.ttf"
+  },
+  {
+    "revision": "304f31f4585cf09768f9d4d69574d2d6",
+    "url": "public/fonts/fa-regular-400.svg"
+  },
+  {
+    "revision": "80efa56be5eaebd06ea34f1adbad071c",
+    "url": "public/fonts/fa-regular-400.eot"
+  },
+  {
+    "url": "js/vendor.eaa8bab80c9f879fe225.js"
+  },
+  {
+    "url": "js/runtime.eaa8bab80c9f879fe225.js"
+  },
+  {
+    "url": "js/main.eaa8bab80c9f879fe225.js"
+  },
+  {
+    "revision": "b43b0708f68e05e25ef0",
+    "url": "css/0.css"
+  }
+];
